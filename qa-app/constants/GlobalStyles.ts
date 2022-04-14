6da6c9a7209a93f990760c08ef6b877b87bd7943
Heihokon <neoshark22@gmail.com>
2022-04-14 16:04:22 +0100

    add globalStyle module

---
+++ qa-app/constants/GlobalStyles.ts
@@ -0,0 +1,16 @@
+const defaultHeight = 40
+const defaultFontSize = 15
+
+export default {
+    textInput:{
+        height: defaultHeight,
+        paddingLeft:5, 
+        paddingEnd:5,
+        borderRadius: 5,
+        fontSize:defaultFontSize
+    },
+    label:{
+        textAlignVertical:'center',
+        fontSize:defaultFontSize
+    }
+}
