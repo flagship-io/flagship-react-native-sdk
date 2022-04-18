049370898a75ec7862dbbf2b7a6ba30a58141333
Heihokon <neoshark22@gmail.com>
2022-04-18 16:48:06 +0100

    add LineContainerInputReadyOnlyText component

---
+++ qa-app/components/LineContainerInputReadyOnlyText.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import GlobalStyles from "../constants/GlobalStyles";
+import { LineContainerInputTextReadyOnlyProps } from "../types";
+import {View, Text, TextInput} from './Themed'
+
+
+function LineContainerInputReadyOnlyText(
+        props: LineContainerInputTextReadyOnlyProps
+    ) {
+        return (
+            <View style={styles.lineContainer}>
+                <Text style={[styles.lineLabel, GlobalStyles.label]}>{props.label}</Text>
+                <TextInput
+                    multiline
+                    editable={false}
+                    selectTextOnFocus={false}
+                    style={[{minHeight:props.height}, styles.lineInputText, GlobalStyles.textInput]}
+                    value={props.value}
+                />
+            </View>
+        );
+    }
+
+const styles = StyleSheet.create({
+        lineContainer: {
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            marginBottom: 10
+        },
+        lineInputText: {
+            flex: 2
+        },
+        lineLabel: {
+            flex: 1,
+        },
+    
+    });
+    
+
+
+export default React.memo(LineContainerInputReadyOnlyText)
