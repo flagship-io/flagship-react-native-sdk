cab02ffe533982bff05165b650d4aefb2652d4e2
Heihokon <neoshark22@gmail.com>
2022-12-13 12:17:54 +0100

    refactor DefaultHitCache

--- src/cache/DefaultHitCache.ts
+++ src/cache/DefaultHitCache.ts
@@ -3,27 +3,34 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const FS_HIT_PREFIX = 'FS_DEFAULT_HIT_CACHE_';
 export class DefaultHitCache implements IHitCacheImplementation {
-    async cacheHit(visitorId: string, data: HitCacheDTO): Promise<void> {
-        const localDatabase = await AsyncStorage.getItem(
-            FS_HIT_PREFIX + visitorId
-        );
-        let dataJson = '';
-        if (localDatabase) {
-            const localData = localDatabase.slice(0, -1);
-            dataJson = `${localData},${JSON.stringify(data)}]`;
-        } else {
-            dataJson = `[${JSON.stringify(data)}]`;
-        }
-        await AsyncStorage.setItem(FS_HIT_PREFIX + visitorId, dataJson);
+    async cacheHit(hits: Record<string, HitCacheDTO>): Promise<void> {
+        const localDatabaseJson = await AsyncStorage.getItem(FS_HIT_PREFIX)||'{}';
+        const localDatabase = JSON.parse(localDatabaseJson)
+
+        const newLocalDatabase = {
+            ...localDatabase,
+            ...hits
+          }
+          
+        await AsyncStorage.setItem(FS_HIT_PREFIX, JSON.stringify(newLocalDatabase))
     }
 
-    async lookupHits(visitorId: string): Promise<HitCacheDTO[]> {
-        const data = await AsyncStorage.getItem(FS_HIT_PREFIX + visitorId);
-        await AsyncStorage.removeItem(FS_HIT_PREFIX + visitorId);
-        return data ? JSON.parse(data) : null;
+    async lookupHits(): Promise<Record<string, HitCacheDTO>> {
+        const localDatabaseJson = await AsyncStorage.getItem(FS_HIT_PREFIX) || '{}'
+        return JSON.parse(localDatabaseJson)
+    }
+    async flushHits(hitKeys: string[]): Promise<void> {
+        const localDatabaseJson = await AsyncStorage.getItem(FS_HIT_PREFIX) || '{}'
+        const localDatabase:Record<string, HitCacheDTO> = JSON.parse(localDatabaseJson)
+    
+        hitKeys.forEach(key => {
+          delete localDatabase[key]
+        })
+    
+        await AsyncStorage.setItem(FS_HIT_PREFIX, JSON.stringify(localDatabase))
     }
 
-    async flushHits(visitorId: string): Promise<void> {
-        await AsyncStorage.removeItem(FS_HIT_PREFIX + visitorId);
+    async flushAllHits(): Promise<void> {
+        await AsyncStorage.removeItem(FS_HIT_PREFIX);
     }
 }
