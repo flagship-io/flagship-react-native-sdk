d3668e4b1eeb91ea4aa774dd789c60b7fd306f84
Heihokon <neoshark22@gmail.com>
2024-04-30 14:47:23 -0500

    Add ItemImage component for React Native app

---
+++ demo/src/ItemImage.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {View, Image, StyleSheet} from 'react-native';
+
+type ItemImageProps = {
+  imageUrl: string;
+  imageAlt: string;
+};
+
+export const ItemImage = ({imageUrl, imageAlt}: ItemImageProps) => {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.image}
+        source={{uri: imageUrl}}
+        accessibilityLabel={imageAlt}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    marginBottom: 16,
+  },
+  image: {
+    width: '80%',
+    height: 200,
+    resizeMode: 'contain',
+  },
+});
