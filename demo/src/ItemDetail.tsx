bc337858340a1a6aee20c7588ee77e12259566c6
Heihokon <neoshark22@gmail.com>
2024-04-30 14:47:19 -0500

    Add ItemDetail component for React Native app

---
+++ demo/src/ItemDetail.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+type ItemDetailProps = {
+  title: string;
+  subtitle: string;
+  refNumber: string;
+};
+
+export function ItemDetail({title, subtitle, refNumber}: ItemDetailProps) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.subtitle}>{subtitle}</Text>
+      <Text style={styles.refNumber}>{refNumber}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '500',
+  },
+  subtitle: {
+    fontSize: 20,
+    fontWeight: '500',
+  },
+  refNumber: {
+    fontSize: 14,
+    color: '#929295',
+  },
+});
