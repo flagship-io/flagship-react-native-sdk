c5f3a20ecdd7c685fb1d7e9c41f97d44089b4dca
Heihokon <neoshark22@gmail.com>
2024-05-01 13:12:04 -0500

    Refactor DiscountedPrice component to include a container view and update styles in DiscountedPrice.tsx

--- demo/src/DiscountedPrice.tsx
+++ demo/src/DiscountedPrice.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, StyleSheet} from 'react-native';
+import {Text, StyleSheet, View} from 'react-native';
 
 type DiscountedPriceProps = {
   price: string;
@@ -8,10 +8,10 @@ type DiscountedPriceProps = {
 
 export function DiscountedPrice({price, discountPrice}: DiscountedPriceProps) {
   return (
-    <>
+    <View style={styles.container}>
       <Text style={styles.price}>{price}</Text>
       <Text style={styles.discountPrice}>{discountPrice}</Text>
-    </>
+    </View>
   );
 }
 
@@ -20,6 +20,11 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: '500',
     textDecorationLine: 'line-through',
+    color: '#333',
+  },
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   discountPrice: {
     fontSize: 24,
