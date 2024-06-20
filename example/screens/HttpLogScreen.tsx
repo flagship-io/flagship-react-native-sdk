e8f937d3fa7df0def0126ca17019cce233f805c2
Heihokon <neoshark22@gmail.com>
2024-06-20 10:29:08 -0500

    chore: Remove unused files and code

--- example/screens/HttpLogScreen.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import NetworkLogger from 'react-native-network-logger';
-
-function HttpLogScreen(){
-    return (<NetworkLogger theme={'dark'} />)
-}
-
-export default HttpLogScreen;
