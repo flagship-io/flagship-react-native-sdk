0ec1aa13342e164ebaea423532778cbb969f3f2a
Heihokon <neoshark22@gmail.com>
2024-11-14 15:43:52 -0600

    feat: Integrate TouchCaptureProvider into FlagshipProvider for enhanced touch event handling

--- src/FlagshipProvider.tsx
+++ src/FlagshipProvider.tsx
@@ -1,8 +1,11 @@
 import {
     FlagshipProvider as ReactFlagshipProvider,
     FlagshipProviderProps as ReactFlagshipProviderProps,
-    VisitorData,Flagship, OS_NAME, OS_VERSION_CODE,
-    CacheStrategy,
+    VisitorData,
+    Flagship,
+    OS_NAME,
+    OS_VERSION_CODE,
+    CacheStrategy
 } from '@flagship.io/react-sdk';
 import React, { useEffect, useState } from 'react';
 import { DefaultHitCache } from './cache/DefaultHitCache';
@@ -10,47 +13,60 @@ import { DefaultVisitorCache } from './cache/DefaultVisitorCache';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Platform } from 'react-native';
 import { version as SDK_VERSION } from './sdkVersion';
+import TouchCaptureProvider from './TouchCaptureProvider';
 
+export const DEFAULT_TIME_INTERVAL = 5;
+export const DEFAULT_POOL_MAX_SIZE = 10;
 
-export const DEFAULT_TIME_INTERVAL = 5
-export const DEFAULT_POOL_MAX_SIZE = 10
+export interface FlagshipProviderProps
+    extends Omit<
+        ReactFlagshipProviderProps,
+        'reuseVisitorIds' | 'nextFetchConfig' | 'sdkVersion' | 'language'
+    > {}
 
-export interface FlagshipProviderProps extends Omit<ReactFlagshipProviderProps, 'reuseVisitorIds'|'nextFetchConfig'|'sdkVersion'|"language"> {}
+// Predefined context keys
+export const SDK_FIRST_TIME_INIT = 'sdk_firstTimeInit';
 
-// Predefined context keys 
-export const SDK_FIRST_TIME_INIT    = "sdk_firstTimeInit"
+export const FlagshipProvider: React.FC<FlagshipProviderProps> = ({
+    children,
+    visitorCacheImplementation,
+    hitCacheImplementation,
+    visitorData,
+    ...props
+}) => {
+    const [newVisitorData, setNewVisitorData] = useState<VisitorData | null>(
+        visitorData
+    );
 
-export const FlagshipProvider: React.FC<FlagshipProviderProps> = ({ children, visitorCacheImplementation, hitCacheImplementation, visitorData, ...props }) => {
-    
-    const [newVisitorData, setNewVisitorData] = useState<VisitorData|null>(visitorData)
-
-    useEffect(()=>{
-        async function loadPredefinedContext(){
-            let firstTimeInit = null
+    useEffect(() => {
+        async function loadPredefinedContext() {
+            let firstTimeInit = null;
             try {
-                firstTimeInit = await AsyncStorage.getItem(SDK_FIRST_TIME_INIT)
-
+                firstTimeInit = await AsyncStorage.getItem(SDK_FIRST_TIME_INIT);
             } catch (error) {
-                Flagship.getConfig()?.logManager?.error("Error on get item from AsyncStorage", "loadPredefinedContext") 
+                Flagship.getConfig()?.logManager?.error(
+                    'Error on get item from AsyncStorage',
+                    'loadPredefinedContext'
+                );
             }
 
             setNewVisitorData({
-                ...visitorData as VisitorData,
+                ...(visitorData as VisitorData),
                 id: Flagship.getVisitor()?.visitorId,
-                context:{
+                context: {
                     ...visitorData?.context,
                     [OS_NAME]: Platform.OS,
-                    [OS_VERSION_CODE]:Platform.Version?.toString(),
+                    [OS_VERSION_CODE]: Platform.Version?.toString(),
                     [SDK_FIRST_TIME_INIT]: !firstTimeInit
                 }
-            })
-            AsyncStorage.setItem(SDK_FIRST_TIME_INIT, SDK_FIRST_TIME_INIT)
+            });
+            AsyncStorage.setItem(SDK_FIRST_TIME_INIT, SDK_FIRST_TIME_INIT);
         }
-        if(visitorData){
-            loadPredefinedContext()
+        if (visitorData) {
+            loadPredefinedContext();
         }
-    },[JSON.stringify(visitorData)])
-    
+    }, [JSON.stringify(visitorData)]);
+
     return (
         <ReactFlagshipProvider
             {...props}
@@ -71,7 +87,7 @@ export const FlagshipProvider: React.FC<FlagshipProviderProps> = ({ children, vi
             }
             visitorData={newVisitorData}
         >
-            <>{children}</>
+            <TouchCaptureProvider>{children}</TouchCaptureProvider>
         </ReactFlagshipProvider>
     );
 };
