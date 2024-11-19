02bb52392dd007c6879474f8bb5d4bffc3011818
Heihokon <neoshark22@gmail.com>
2024-11-18 18:52:31 -0600

    feat: Refactor TouchCaptureProvider to enhance touch event handling and add click path tracking

--- src/TouchCaptureProvider.tsx
+++ src/TouchCaptureProvider.tsx
@@ -1,92 +1,127 @@
-import { Flagship, IVisitorEvent } from "@flagship.io/react-sdk";
-import React, { useRef } from "react";
 import {
-  View,
-  GestureResponderEvent,
-  StyleSheet,
-  Dimensions,
-} from "react-native";
+    Flagship,
+    IVisitorEvent,
+    FlagshipContext
+} from '@flagship.io/react-sdk';
+import React, { useContext, useRef } from 'react';
+import {
+    View,
+    GestureResponderEvent,
+    StyleSheet,
+    Dimensions
+} from 'react-native';
+import { MAX_CLICK_PATH_LENGTH } from './Constant';
 
 interface TouchCaptureProviderProps {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }
 
 const TouchCaptureProvider: React.FC<TouchCaptureProviderProps> = ({
-  children,
+    children
 }) => {
-  const lastTapEvent = useRef<GestureResponderEvent | null>(null);
-  const lastTapEventTime = useRef<number | null>(null);
-  const visitor = Flagship.getVisitor();
+    const lastTapEvent = useRef<GestureResponderEvent | null>(null);
+    const lastTapEventTime = useRef<number | null>(null);
+    const clickPath = useRef<string>('');
+    const clickPathTimeoutId = useRef<NodeJS.Timeout | null>(null);
+    const { state } = useContext(FlagshipContext);
 
+    function sendTouchPositionEvent(event: GestureResponderEvent) {
+        const visitor = Flagship.getVisitor();
+        if (!visitor) {
+            return null;
+        }
 
-  function sendTapEvent(event: GestureResponderEvent) {
-    if (!visitor) {
-      return null;
+        const { pageX, pageY } = event.nativeEvent;
+        const screen = Dimensions.get('screen');
+        const timestamp = Date.now().toString().slice(-5);
+        const visitorEvent: IVisitorEvent = {
+            customerAccountId: Flagship.getConfig().envId as string,
+            visitorId: visitor.visitorId as string,
+            currentUrl: '',
+            clickPosition: `${pageY},${pageX},${timestamp},0;`,
+            screenSize: `${screen.width},${screen.height};`
+        };
+
+        (visitor as any).sendEaiVisitorEvent(visitorEvent);
     }
-  
-    const { pageX, pageY } = event.nativeEvent;
-    console.log("sendTapEvent", pageX, pageY);
-    const screen = Dimensions.get("screen");
-    const timestamp = Date.now().toString().slice(-5)
-    const visitorEvent: IVisitorEvent = {
-      customerAccountId: Flagship.getConfig().envId as string,
-      visitorId: visitor.visitorId as string,
-      currentUrl: "",
-      clickPosition: `${pageY},${pageX},${timestamp},0;`,
-      screenSize:`${screen.width},${screen.height};`,
-    };
 
-    (visitor as any).sendEaiVisitorEvent(visitorEvent);
-
-  }
-
-  function sendMoveEvent(event: GestureResponderEvent) {
-    const { pageX, pageY } = event.nativeEvent;
-    console.log("sendMoveEvent", pageX, pageY);
-    // Add your move handling logic here
-  }
-
-  function handleTouchStart(event: GestureResponderEvent) {
-    if (lastTapEvent.current) {
-      sendTapEvent(lastTapEvent.current);
+    function sendTouchPathEvent(): void {
+        const screen = Dimensions.get('screen');
+        const visitor = Flagship.getVisitor();
+        const visitorId = visitor?.visitorId as string;
+        const visitorEvent: IVisitorEvent = {
+            visitorId,
+            customerAccountId: Flagship.getConfig().envId as string,
+            clickPath: clickPath.current,
+            screenSize: `${screen.width},${screen.height};`,
+            currentUrl: ''
+        };
+        clickPath.current = '';
+        (visitor as any).sendEaiVisitorEvent(visitorEvent);
     }
-    lastTapEventTime.current = Date.now();
-    lastTapEvent.current = event;
-  }
 
-  function handleTouchMove(event: GestureResponderEvent) {
-    if (
-      lastTapEventTime.current &&
-      Date.now() - lastTapEventTime.current < 500
-    ) {
-      lastTapEventTime.current = null;
-      lastTapEvent.current = null;
+    function processMoveEvent(event: GestureResponderEvent) {
+        const { pageX, pageY } = event.nativeEvent;
+
+        console.log('sendMoveEvent', pageX, pageY);
+
+        if (clickPathTimeoutId.current) {
+            clearTimeout(clickPathTimeoutId.current);
+        }
+        clickPath.current += `${pageY},${pageX},${Date.now()
+            .toString()
+            .slice(-5)};`;
+
+        if (clickPath.current.length > MAX_CLICK_PATH_LENGTH) {
+            sendTouchPathEvent();
+        }
+
+        clickPathTimeoutId.current = setTimeout(() => {
+            sendTouchPathEvent();
+        }, 500);
     }
-    sendMoveEvent(event);
-  }
 
-  return (
-    <View
-      style={styles.container}
-      pointerEvents="box-none"
-      onTouchStart={handleTouchStart}
-      onTouchMove={handleTouchMove}
-    >
-      {children}
-    </View>
-  );
+    function handleTouchStart(event: GestureResponderEvent) {
+        if (lastTapEvent.current) {
+            sendTouchPositionEvent(lastTapEvent.current);
+        }
+        lastTapEventTime.current = Date.now();
+        lastTapEvent.current = event;
+    }
+
+    function handleTouchMove(event: GestureResponderEvent) {
+        if (
+            lastTapEventTime.current &&
+            Date.now() - lastTapEventTime.current < 500
+        ) {
+            lastTapEventTime.current = null;
+            lastTapEvent.current = null;
+        }
+        processMoveEvent(event);
+    }
+
+    return (
+        <View
+            style={styles.container}
+            pointerEvents="box-none"
+            onTouchStart={handleTouchStart}
+            onTouchMove={handleTouchMove}
+        >
+            {children}
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    position: "absolute", // Overlay on top
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: "transparent", // Ensure it's transparent
-    zIndex: 10000, // Ensure it's on top
-  },
+    container: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundColor: 'transparent',
+        zIndex: 10000
+    }
 });
 
 export default TouchCaptureProvider;
