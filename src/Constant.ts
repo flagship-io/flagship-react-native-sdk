7895d187f4dd8c0eb1c1f619ad0e9579c7a3d8fe
Heihokon <neoshark22@gmail.com>
2025-01-15 11:30:55 -0600

    feat: Add new constants for client cache and SDK initialization parameters

--- src/Constant.ts
+++ src/Constant.ts
@@ -1,2 +1,6 @@
 export const MAX_CLICK_PATH_LENGTH = 1900
 export const TIMEOUT_DURATION = 500;
+export const CLIENT_CACHE_KEY = 'FS_CLIENT_VISITOR';
+export const DEFAULT_TIME_INTERVAL = 5;
+export const DEFAULT_POOL_MAX_SIZE = 10;
+export const SDK_FIRST_TIME_INIT = 'sdk_firstTimeInit';
